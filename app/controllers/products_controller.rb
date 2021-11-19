3777ee0074e2f03498c0c6c6e7ab3de3916ccf73
Hasan Ozovali <ozovalihasan@gmail.com>
2021-11-19 17:48:10 +0300

    Add turbo_stream.erb template for Products#destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,6 +71,7 @@ class ProductsController < ApplicationController
     @product.destroy
     respond_to do |format|
       format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
+      format.turbo_stream
     end
   end
 
