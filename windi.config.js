4136bbeac8202b06f66056d2195981a3898f7a6f
Hasan Ozovali <ozovalihasan@gmail.com>
2021-12-01 15:32:19 +0300

    Update shortcuts  used for windicss

--- windi.config.js
+++ windi.config.js
@@ -15,7 +15,8 @@ export default defineConfig({
   shortcuts: {
     'btn': 'py-2 px-4 font-semibold rounded-lg text-shadow-md bg-transparent cursor-pointer text-center',
     'btn-primary': 'py-2 px-4 font-semibold rounded-lg border bg-first-500 hover:bg-first-600 text-white cursor-pointer text-center block',
-    'btn-secondary': 'py-2 px-4 font-semibold rounded-lg border hover:bg-white bg-gray cursor-pointer text-center block',
+    'btn-secondary': 'py-2 px-4 font-semibold rounded-lg border bg-white hover:bg-gray-100 cursor-pointer text-center block',
+    'link-primary': 'text-first-500 hover:text-first-600',
     'btn-warning': 'py-2 px-4 font-semibold rounded-lg border bg-warning-500 hover:bg-warning-600 text-white cursor-pointer text-center block',
     'input-primary': 'bg-first-50 placeholder-black-900 outline-none p-2 w-full',
     'center-content': 'flex items-center justify-center'
